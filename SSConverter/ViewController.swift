47f7100300553d02e9e61abc1bb6987c98f2628e
Astun <caravaca@gmail.com>
2014-09-22 18:28:26 +0200

    Initial Commit

---
+++ SSConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SSConverter
+//
+//  Created by Antonio Caravaca Vega on 22/09/14.
+//  Copyright (c) 2014 ACV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

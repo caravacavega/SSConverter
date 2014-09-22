fa172df0c4897e336eba06815482216f6e2206cb
Astun <caravaca@gmail.com>
2014-09-22 20:35:46 +0200

    Lesson Finished

--- SSConverter/ViewController.swift
+++ SSConverter/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menSizeTextField: UITextField!
+    @IBOutlet weak var convertedLabel: UILabel!
+    @IBOutlet weak var womenSizeTextField: UITextField!
+    @IBOutlet weak var womanConvertedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ConvertButtonPressed(sender: UIButton) {
+        let sizeFromTextField = menSizeTextField.text.toInt()!
+        let conversionConstant = 30
+        let convertedSizeString = "\(sizeFromTextField + conversionConstant)"
+        convertedLabel.hidden = false
+        convertedLabel.text = convertedSizeString + " in European Shoe Size"
+    }
 
+    @IBAction func WomanConvertButtonPressed(sender: UIButton) {
+        let sizeFromTextField = Double ((womenSizeTextField.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        let convertedSizeString = "\(sizeFromTextField + conversionConstant)"
+        womanConvertedLabel.hidden = false
+        womanConvertedLabel.text = convertedSizeString + " in European Shoe Size"
+    }
+    
 }
 
